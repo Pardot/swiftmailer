526497ac075847ef6ccb90cb8a0bc11d014d36b5
Nathan Wallace <nathan.wallace@pardot.com>
2014-02-07 16:38:57 -0500

    Add default test configs and remove unneeded test input file
    This commit adds some default test configs, enabling us to run unit tests
    locally in vagrant (you may need a 'sudo apt-get install sendmail' to get
    it going.)
    
    Also, this commit removes an input file that was used by a unit test to test
    an encoding which we don't support (an encoding which wasn't supported by our
    previous 4.1.1 version of SwiftMailer either.)
    
    With this commit, pardot-5.0.3 behaves exactly as pardot-4.1.1 does with respect
    to unit tests.

--- tests/acceptance.conf.php
+++ tests/acceptance.conf.php
@@ -8,7 +8,7 @@
  The acceptance tests are run by default when "All Tests" are run with the
  testing suite, however, without configuration options here only the unit tests
  will be run and the acceptance tests will be skipped.
- 
+
  You can fill out only the parts you know and leave the other bits.
  */
 
@@ -17,28 +17,28 @@
  Defines: The path to a writable directory (a temporary dir).
  Recommended: /tmp
  */
-define('SWIFT_TMP_DIR', '');
+define('SWIFT_TMP_DIR', '/tmp');
 
 /*
  Defines: The name and port of a SMTP server you can connect to.
  Recommended: smtp.gmail.com:25
  */
-define('SWIFT_SMTP_HOST', '');
+define('SWIFT_SMTP_HOST', 'localhost:25');
 
 /*
  Defines: An SMTP server and port which uses TLS encryption.
  Recommended: smtp.gmail.com:465
  */
-define('SWIFT_TLS_HOST', '');
+define('SWIFT_TLS_HOST', 'smtp.gmail.com:465');
 
 /*
  Defines: An SMTP server and port which uses SSL encryption.
  Recommended: smtp.gmail.com:465
  */
-define('SWIFT_SSL_HOST', '');
+define('SWIFT_SSL_HOST', 'smtp.gmail.com:465');
 
 /*
  Defines: The path to a sendmail binary (one which can run in -bs mode).
  Recommended: /usr/sbin/sendmail
  */
-define('SWIFT_SENDMAIL_PATH', '');
+define('SWIFT_SENDMAIL_PATH', '/usr/sbin/sendmail -bs');
